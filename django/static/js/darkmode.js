6fb0eab2b3d45fa2e49a5424bab530f8fe78e53a
efaideleon <eedeleon@uci.edu>
2020-01-09 17:31:38 -0800

    header changes

--- django/static/js/darkmode.js
+++ django/static/js/darkmode.js
@@ -1,12 +1,12 @@
 let button = document.querySelector("#theme-button");
 
+function buttonPressedTheme(){
+    button.style.color = '#ff00ff';
+}
 function buttonLightTheme(){
     button.innerHTML = "&#9788;";
     button.style.backgroundColor = 'white';
     button.style.color = 'black';
-}
-function buttonPressedTheme(){
-    button.style.color = '#ff00ff';
 }
 function buttonDarkTheme(){
     button.innerHTML = "&#9789;";
@@ -14,22 +14,23 @@ function buttonDarkTheme(){
     button.style.color = 'white';
 }
 
-if(localStorage.getItem("key") === "true"){
+if(localStorage.getItem("isDarkModeOn") === "true"){
     document.documentElement.setAttribute('data-theme', 'dark');
     buttonDarkTheme();
 }else{
     document.documentElement.setAttribute('data-theme', 'light');
     buttonLightTheme();
 }
+
 function themeChange() {
     if(document.documentElement.getAttribute('data-theme') === 'dark'){ 
         document.documentElement.setAttribute('data-theme', 'light');
         buttonLightTheme();
-        localStorage.setItem("key", "false");
+        localStorage.setItem("isDarkModeOn", "false");
     }else{
         document.documentElement.setAttribute('data-theme', 'dark');
         buttonDarkTheme();
-        localStorage.setItem("key", "true");
+        localStorage.setItem("isDarkModeOn", "true");
     }
 }
 button.addEventListener('mousedown', buttonPressedTheme);
