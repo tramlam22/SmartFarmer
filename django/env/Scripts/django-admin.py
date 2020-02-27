41e87b384009735ab988b35d219b2df1d071177c
efaideleon <eedeleon@uci.edu>
2020-02-27 13:16:22 -0800

    removed env folder

--- django/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\tramp\appdata\local\programs\python\python36\eecs159a\smart_planter\django\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
