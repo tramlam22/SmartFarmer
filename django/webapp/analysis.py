e304fe29d306b21ecd12d3e734fd7cc81919c58c
efaideleon <eedeleon@uci.edu>
2020-03-06 12:33:00 -0800

    more algorithm stuff

--- django/webapp/analysis.py
+++ django/webapp/analysis.py
@@ -5,31 +5,49 @@ class dataAnalysis():
     
     def tempAlgorithm(self, average_temp):
         if(average_temp > 20.0 and average_temp < 29 ):
-            suggestion_message = "Temperature is great!"
+            feedback_message = "Temperature is great!"
         elif(average_temp < 20.0):        
-            suggestion_message = "Your temperature is not ideal! the temperature is too low!"
+            feedback_message = "Your temperature is not ideal! the temperature is too low!"
         elif(average_temp > 29):
-            suggestion_message = "Your temperature is not ideal! the temperature is too high!"
-        return suggestion_message
+            feedback_message = "Your temperature is not ideal! the temperature is too high!"
+        return feedback_message
     
     def humidityAlgorithm(self, average_humidity):
         if(average_humidity > 65.0 and average_humidity < 75 ):
-            suggestion_message = "Humidity level is good!"
+            feedback_message = "Humidity level is good!"
         elif(average_humidity < 65.0):        
-            suggestion_message = "Humidity level is not ideal! Humidity is too low!"
+            feedback_message = "Humidity level is not ideal! Humidity is too low!"
         elif(average_humidity > 75):
-            suggestion_message = "Humidity level is not ideal! Humidity is too high!"
-        return suggestion_message
+            feedback_message = "Humidity level is not ideal! Humidity is too high!"
+        return feedback_message
     
 
     def lightAlgorithm(self, average_light_reading):
-        pass
+        if(average_light_reading > 65.0 and average_light_reading < 75 ):
+            feedback_message = "Light level is good!"
+        elif(average_light_reading < 65.0):        
+            feedback_message = "Light level is not ideal! Light is too low!"
+        elif(average_light_reading > 75):
+            feedback_message = "Light level is not ideal! Light is too high!"
+        return feedback_message
 
     def soilMoistureAlgorithm(self, average_soil_moisture):
-        pass
+        if(average_soil_moisture > 65.0 and average_soil_moisture < 75 ):
+            feedback_message = "Soil moisture level is good!"
+        elif(average_soil_moisture < 65.0):        
+            feedback_message = "Soil moisture level is not ideal! Soil moisture is too low!"
+        elif(average_soil_moisture > 75):
+            feedback_message = "Soil moisture level is not ideal! Soil moisture is too high!"
+        return feedback_message
 
     def soilTempAlgorithm(self, average_soil_temp):
-        pass
+        if(average_soil_temp > 65.0 and average_soil_temp < 75 ):
+            feedback_message = "Soil temperature level is good!"
+        elif(average_soil_temp < 65.0):        
+            feedback_message = "Soil temperature level is not ideal! Soil temperature is too low!"
+        elif(average_soil_temp > 75):
+            feedback_message = "Soil temperature level is not ideal! Soil temperature is too high!"
+        return feedback_message
 
     def algorithm(self):
         number_of_entries = 10
@@ -92,6 +110,40 @@ class dataAnalysis():
             suggestion_message = "Irrigate more to yield optimal strawberries!"
             notification = "Low humidity level!"
         
+
+        feedback += "\n"
+        feedback += self.lightAlgorithm(average_light_reading)
+        if "low" in feedback:
+            suggestion_message = "Increase temperature to yield optimal strawberries!"
+            notification = "Temperature is below optimal!"
+        if "high" in feedback:
+            suggestion_message = "Lower temperature to yield optimal strawberries!"
+            notification = "Temperature is too high!"
+
+
+
+        feedback += "\n"
+        feedback += self.soilMoistureAlgorithm(average_soil_moisture)
+
+        if "low" in feedback:
+            suggestion_message = "Increase temperature to yield optimal strawberries!"
+            notification = "Temperature is below optimal!"
+        if "high" in feedback:
+            suggestion_message = "Lower temperature to yield optimal strawberries!"
+            notification = "Temperature is too high!"
+
+        feedback += "\n"
+        feedback += self.soilTempAlgorithm(average_soil_temp)
+
+        if "low" in feedback:
+            suggestion_message = "Increase temperature to yield optimal strawberries!"
+            notification = "Temperature is below optimal!"
+        if "high" in feedback:
+            suggestion_message = "Lower temperature to yield optimal strawberries!"
+            notification = "Temperature is too high!"
+
+
+
         if "ideal" in feedback:
             sendAlert("efai",feedback)
             
