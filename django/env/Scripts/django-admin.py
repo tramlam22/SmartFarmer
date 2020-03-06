2af27702dc54336db31f06d5bff645b1b823d398
tramlam22 <tramphuonglam@gmail.com>
2020-03-05 20:03:01 -0800

    uwu

---
+++ django/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tramp\appdata\local\programs\python\python36\eecs159a\smart_planter\django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- django/webapp/plots.py
+++ django/webapp/plots.py
@@ -14,8 +14,17 @@ class sensorData():
 
     # set user to find the appropriate data
     def __init__(self, current_user):
+        
+        cursor = connection.cursor()
+        
         self.user = current_user
-        #self.moduleList = [module.mcu_no for module in modulePlantLink.objects.filter(username=self.user)]
+        cmd = """SELECT mcu_no
+                FROM modulePlantlink
+                WHERE username = {}""".format(self.user)
+
+        cursor.execute(cmd)
+        self.moduleList = cursor.fetchall()
+        cursor.close()
 
     # get avg graphs for certain time intervals
     # default page should be at hours
@@ -73,36 +82,65 @@ class sensorData():
         return plot_div
 
     # get graphs for each module
-    def moduleGraph(self, typeofData, timeInterval):
-        for module in moduleList:
-            cmd = """SELECT AVG({})
-                    FROM dataMCU
-                    WHERE mcu_no = {}
-                    GROUP BY HOUR(date_data)""".format(typeofData, module)
+    def moduleGraph(self, moduleNum, typeofData, timeInterval):
+
+        cursor = connection.cursor()
+        
+        if timeInterval == "hour":
+            cmd = """SELECT data_date, AVG({})
+                    FROM dataMCU 
+                    WHERE mcu_no = {} ANDCURDATE() = DATE(data_date)
+                    GROUP BY DATE(data_date), HOUR(data_date)""".format(typeofData, moduleNum)
+        elif timeInterval == "day":
+            cmd = """SELECT data_date, AVG({})
+                    FROM dataMCU 
+                    WHERE mcu_no = {} AND MONTH(CURDATE()) = MONTH(data_date)
+                    GROUP BY DATE(data_date)""".format(typeofData, moduleNum)
+        elif timeInterval == "months":
+            cmd = """SELECT data_date, AVG({})
+                    FROM dataMCU 
+                    WHERE mcu_no = {} AND YEAR(CURDATE()) = YEAR(data_date)
+                    GROUP BY MONTH(data_date)""".format(typeofData, moduleNum)
+
         cursor.execute(cmd)
         moduleData = cursor.fetchall()
         x = [row[0] for row in moduleData]
+        y = [row[1] for row in moduleData]
 
-    # for testing purposes
+        cursor.close()
 
-    def createTestGraph(self):
-        data = Testtemp.objects.all()
-        x = [row.id for row in data]
-        y = [row.temp for row in data]
+        if typeofData == "temp": typeofData = "Air Temperature for Module {}".format(moduleNum)
+        elif typeofData == "soil_temp": typeofData = "Soil Temperature {}".format(moduleNum)
+        elif typeofData == "humidity": typeofData = "Humidity {}".format(moduleNum)
+        elif typeofData == "soil_moisture": typeofData = "Soil Moisture {}".format(moduleNum)
 
         figure = go.Figure()
-        figure.add_trace(go.Scatter(x=x, y=y, mode='lines',
-                                    name="Testing",
-                                    line=dict(color="#ff0000", width=4),
-                                    connectgaps=True,
-                                    ))
+        figure.add_trace(
+            go.Scatter(
+                x=x, y=y, mode='lines',
+                name=typeofData,
+                line=dict(color="#003366", width=4),
+                connectgaps=True,
+            )
+        )
+        figure.update_layout(
+            title = typeofData,
+            xaxis_title = "Date and Time",
+            font=dict(
+                family="Courier New, monospace",
+                size=12
+            )
+        )
 
         plot_div = plot(figure, output_type='div', include_plotlyjs=False)
         return plot_div
 
+        cursor.close()
+
+
     # getting all data, delete after
-    def getAllData(self):
+    """def getAllData(self):
         data = dataMCU.objects.values(
             'data_date', 'soil_temp', 'soil_moisture', 'temp', 'humidity').order_by('-data_date')
         x = data
-        return x
+        return x"""
