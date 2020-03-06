2dab2fe7bcbcc24d2283d3c5e5ddec6ae8fa135a
efaideleon <eedeleon@uci.edu>
2020-03-05 20:44:49 -0800

    small changes

--- django/webapp/analysis.py
+++ django/webapp/analysis.py
@@ -28,6 +28,7 @@ class dataAnalysis():
         soil_moisture = []                              #soil_moisture from the last 'number' of entries
         temp = []                                       #temp from the last 'number' of entries
         humidity = []                                   #humidity from the last 'number' of entries
+        light_reading = []
         recent_data = []                                #data from the last 'number' of  entries 
         sensor_data = sensorData("farm")
         all_data = sensorData.getAllData(self)          #getting all the data base entries in the form of a list
@@ -35,6 +36,7 @@ class dataAnalysis():
         average_soil_temp = 0
         average_soil_moisture = 0
         average_humidity = 0
+        average_light_reading = 0
         recent_date = ""
         suggestion_message = ""
       
@@ -48,17 +50,20 @@ class dataAnalysis():
             temp.append(recent_data[i]['temp'])
             humidity.append(recent_data[i]['humidity'])
             soil_moisture.append(recent_data[i]['soil_moisture'])  
-    
+            light_reading.append(recent_data[i]['light_reading'])
+
         for i in range(number_of_entries):                  #calculating averages
             average_temp += temp[i]
             average_soil_temp += soil_temp[i]
             average_soil_moisture += soil_moisture[i]
             average_humidity += humidity[i]
+            average_light_reading += light_reading[i]
 
         average_temp /= number_of_entries
         average_soil_temp /= number_of_entries
         average_soil_moisture /= number_of_entries
         average_humidity /= number_of_entries 
+        average_light_reading /= number_of_entries
         recent_date = data_date[0]
         
         suggestion_message += self.tempAlgorithm(average_temp)
@@ -66,5 +71,5 @@ class dataAnalysis():
         suggestion_message += self.humidityAlgorithm(average_humidity)
 
 
-        return average_temp, average_soil_temp, average_soil_moisture, average_humidity, recent_date, suggestion_message;
+        return average_temp, average_soil_temp, average_soil_moisture, average_humidity, recent_date, average_light_reading, suggestion_message
     
